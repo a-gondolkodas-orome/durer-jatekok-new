2316bfb61de74eb97e83277d58021f77b94f61eb
radl97 <radl97@users.noreply.github.com>
2022-07-01 15:56:43 +0200

    Make server configurable

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import { Server } from 'boardgame.io/server';
 import { MyGame as TicTacToeGame } from './games/tictactoe/game';
 import { MyGame as SuperstitiousCountingGame } from './games/superstitious-counting/game';
+import { PostgresStore } from 'bgio-postgres';
+import { env } from 'process';
+
+const CONNECTION_STRING = env.DATABASE_URL || "posgresql://postgres:postgres@localhost:5432/postgres";
 
 const server = Server({
     games: [
         TicTacToeGame,
         SuperstitiousCountingGame,
     ],
+    db: new PostgresStore(CONNECTION_STRING),
 })
 
-server.run(8000);
+const PORT = parseInt(env.PORT || "8000");
+server.run(PORT);
